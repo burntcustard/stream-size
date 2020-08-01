c48af3e1ecfc5140f8a2df48c5aa2b5247b12c8c
burntcustard <burntcustard@gmail.com>
2020-08-01 01:50:34 +0100

    A working test\!

--- test.js
+++ test.js
@@ -1,31 +1,19 @@
-var assert = require('assert');
-//var es = require('event-stream');
-var File = require('vinyl');
-var streamSize = require('.');
-var stdout = require("test-console").stdout;
-var gulp = require('gulp');
-var vfs = require('vinyl-fs');
-var fs = require('fs');
-var sinon = require('sinon');
-const log = require('fancy-log');
+const streamSize = require('.');
+const gulp = require('gulp');
+const stdoutSpy = jest.spyOn(process.stdout, 'write');
 
-describe('stream-size', function() {
-  it('should do something', function() {
-
-    const stub = sinon.stub(log);
-
-    console.log(stub);
-    stub.calledWith('test');
-
-    function test() {
-      return gulp
-        .src('test.txt')
-        .pipe(streamSize())
-        .pipe(gulp.dest('test-output'));
-    }
-
-    gulp.series(test);
-
-    //assert.deepEqual(stub.calledWith('test'), ['7 B']);
+describe('stream-size', () => {
+  it('should work with gulp', (done) => {
+    gulp
+      .src('test.txt')
+      .pipe(streamSize())
+      .pipe(gulp.dest('./'))
+      .on('end', function() {
+        expect(stdoutSpy).lastCalledWith(
+          expect.stringContaining('test.txt: 27 B'),
+          expect.anything()
+        );
+        done();
+      });
   });
 });
